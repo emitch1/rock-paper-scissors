af2ca88ca51cd1ff85fda24d3c03afa220f20a8f
emitch1 <eric_mitch@hotmail.com>
2022-06-16 01:39:45 -0400

    complete project need just some clean up

--- javascript.js
+++ javascript.js
@@ -9,9 +9,67 @@ return string that declares winner of round
 
 */
 
+//Function determines randomly which one computer will choose and return that value
 function computerPlay() {
-    let number = Math.floor(Math.random() * 3) + 1;
-    console.log(number);
+    let number = Math.floor(Math.random() * 3) + 1; 
+    if (number == 1) {
+        return 'rock';
+    } else if (number == 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 }
 
-computerPlay();
+function singleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        console.log(`It's a tie!`);
+        return 0;
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        console.log('You lose! Paper beats rock');
+        return -1;
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        console.log('You lose! Scissors beats paper');
+        return -1;
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        console.log('You lose! Rock beats scissors');
+        return -1;
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        console.log('You win! Paper beats rock');
+        return 1;
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        console.log('You win! Scissors beats paper');
+        return 1;
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        console.log('You win! Rock beats scissors');
+        return 1;
+    }
+}
+
+function calculateWinner(score) {
+    if (score == 0) {
+        console.log(`No winners, it's a tie!`);
+    } else if (score > 0) {
+        console.log(`You won with ${score} point(s) more than computer!`);
+    } else {
+        console.log(`You lost with ${score - (score * 2)} point(s) less than computer.`);
+    }
+
+}
+
+// Starts the game, sets score to 0. Asks user for selection and randomizes computer selection.
+// Calls to functions and adds score. Then calls for score function.
+function game() {
+
+    let score = 0;
+    for (let i = 0; i < 5; i++){
+        let playerSelection = prompt('Enter your selection: ');
+        let computerSelection = computerPlay();
+        score += singleRound(playerSelection, computerSelection);
+    }
+    calculateWinner(score);
+}
+
+game();
+
