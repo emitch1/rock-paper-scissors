c385eb06ac2dee72932dad65072961f142a4c19d
emitch1 <eric_mitch@hotmail.com>
2022-07-03 17:53:00 -0400

    Complete UI integration into rock paper scissors

--- javascript.js
+++ javascript.js
@@ -18,48 +18,33 @@ function computerPlay() {
 function singleRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
-        console.log(`It's a tie!`);
+        result.innerHTML= '<span>It is a tie!</span>';
         return '';
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log('You lose! Paper beats rock');
+        result.innerHTML='<span>You lose! Paper beats rock</span>';
         return 'computer';
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        console.log('You lose! Scissors beats paper');
+        result.innerHTML='<span>You lose! Scissors beats paper</span>';
         return 'computer';
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        console.log('You lose! Rock beats scissors');
+        result.innerHTML='<span>You lose! Rock beats scissors</span>';
         return 'computer';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('You win! Paper beats rock');
+        result.innerHTML='<span>You win! Paper beats rock</span>';
         return 'player';
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log('You win! Scissors beats paper');
+        result.innerHTML='<span>You win! Scissors beats paper</span>';
         return 'player';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log('You win! Rock beats scissors');
+        result.innerHTML='<span>You win! Rock beats scissors</span>';
         return 'player';
     }
-}
-
-//Extra helper function to check who won
-function calculateWinner(playerScore, computerScore) {
-    if (playerScore == computerScore) {
-        console.log(`No winners, it's a tie! Both players have ${playerScore} point(s).`);
-    } else if (playerScore > computerScore) {
-        console.log(`You won with ${playerScore} point(s) and computer has ${computerScore} point(s).`);
-    } else {
-        console.log(`You lost with ${playerScore} point(s) and computer has ${computerScore} point(s).`);
-    }
-
 }
 
 // Sets score to 0. Asks user for selection and randomizes computer selection by function call.
 // Calls to functions and adds score. Then calls for score function.
 function game(playerSelection) {
 
-    console.log('test')
-    let playerScore = 0;
-    let computerScore = 0;
     //for (let i = 0; i < 5; i++){
         //let playerSelection = prompt('Enter your selection (Rock, Paper, Scissors): ');
     let computerSelection = computerPlay();
@@ -71,10 +56,33 @@ function game(playerSelection) {
         computerScore++;
     }
     //}
-    calculateWinner(playerScore, computerScore);
+
+    score.innerHTML=`<span>Player: ${playerScore} \xa0 \xa0 \xa0 Computer: ${computerScore} </span>`;
+   
+    if (playerScore == 5) {
+        score.innerHTML+= `<br><br><span>\nCongratulation! You have won the game! Scores are now being reset to 0.</span>`;
+        playerScore = 0;
+        computerScore = 0;
+
+    } else if (computerScore ==5) {
+        score.innerHTML+= `<br><br><span>\nSorry! You have lost! Scores are now being reset to 0.</span>`;
+        playerScore = 0;
+        computerScore = 0;
+
+    }
+
 }
 
 // Starts the game.
+
+let playerScore = 0;
+let computerScore = 0;
+
+const result = document.querySelector('.results');
+
+const score = document.querySelector('.finalResults');
+score.innerHTML=`<span>Player: ${playerScore} \xa0 \xa0 \xa0 Computer: ${computerScore} </span>`;
+
 const rock = document.querySelector('.rock');
 rock.addEventListener('click', () => game('rock'));
 
